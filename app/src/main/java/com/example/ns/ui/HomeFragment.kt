611b893f5c7e4456dd98f1fce4eb9a8ba6817a78
Weaam alagha <32608267+Weaam87@users.noreply.github.com>
2022-12-09 18:56:09 +0800

    add logout icon

--- app/src/main/java/com/example/ns/ui/HomeFragment.kt
+++ app/src/main/java/com/example/ns/ui/HomeFragment.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.ImageView
 import androidx.navigation.fragment.findNavController
 import com.example.ns.R
 import com.google.firebase.auth.ktx.auth
@@ -19,8 +19,7 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home, container, false)
-
-        view.findViewById<Button>(R.id.btn_log_out).setOnClickListener {
+        view.findViewById<ImageView>(R.id.img_log_out).setOnClickListener {
             Firebase.auth.signOut()
             findNavController().navigate(R.id.action_homeFragment_to_loginFragment)
         }
