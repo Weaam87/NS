6e7726efee26cdbba1d9923b9dce53abb75bae06
Weaam alagha <32608267+Weaam87@users.noreply.github.com>
2022-12-09 22:54:49 +0800

    Add Bottom navigation to home screen

--- app/src/main/java/com/example/ns/ui/HomeFragment.kt
+++ app/src/main/java/com/example/ns/ui/HomeFragment.kt
@@ -6,19 +6,50 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.ns.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class HomeFragment : Fragment() {
 
+    private lateinit var bottomNavigation : BottomNavigationView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        bottomNavigation = view.findViewById(R.id.bottom_navigation)
+
+        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.home -> {
+                    // Respond to home click
+                    Toast.makeText(context, "Home", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.support -> {
+                    // Respond to support click
+                    Toast.makeText(context, "Support", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.settings -> {
+                    // Respond to settings click
+                    true
+                }
+                R.id.location -> {
+                    // Respond to location click
+                    true
+                }
+                else -> false
+            }
+        }
+
+
         view.findViewById<ImageView>(R.id.img_log_out).setOnClickListener {
             Firebase.auth.signOut()
             findNavController().navigate(R.id.action_homeFragment_to_loginFragment)
