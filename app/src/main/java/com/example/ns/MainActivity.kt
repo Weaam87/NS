4eb61de3474d578fafa8e8a074637712a890f84a
Weaam alagha <32608267+Weaam87@users.noreply.github.com>
2022-12-10 22:45:58 +0800

    Auto sign out if the user is inactive

--- app/src/main/java/com/example/ns/MainActivity.kt
+++ app/src/main/java/com/example/ns/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.ns
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.MotionEvent
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -12,7 +15,12 @@ import com.google.firebase.ktx.Firebase
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
-    private lateinit var fAuth : FirebaseAuth
+    private lateinit var fAuth: FirebaseAuth
+
+    // Declaring handler, runnable and time in milli seconds
+    private lateinit var mHandler: Handler
+    private lateinit var mRunnable: Runnable
+    private var mTime: Long = 2000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,14 +35,53 @@ class MainActivity : AppCompatActivity() {
         // Open home screen directly if the user already logged in
         if (currentUser != null) {
             navController.navigate(R.id.homeFragment)
-        }else{
+        } else {
             // launch the login fragment
             navController.navigate(R.id.loginFragment)
         }
 
+        // Initializing the handler and the runnable
+        mHandler = Handler(Looper.getMainLooper())
+        mRunnable = Runnable {
+            if (currentUser != null) {
+                fAuth.signOut()
+                navController.navigate(R.id.action_homeFragment_to_loginFragment)
+
+            }
+        }
+
+        //Start the handler
+        startHandler()
+
+
         //This ensures action bar (app bar) buttons, like the menu option are visible.
         setupActionBarWithNavController(navController)
     }
+
+    // When the screen is touched or motion is detected
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+
+        // Removes the handler callbacks (if any)
+        stopHandler()
+
+        // Runs the handler (for the specified time)
+        // If any touch or motion is detected before
+        // the specified time, this override function is again called
+        startHandler()
+
+        return super.onTouchEvent(event)
+    }
+
+    // start handler function
+    private fun startHandler() {
+        mHandler.postDelayed(mRunnable, mTime)
+    }
+
+    // stop handler function
+    private fun stopHandler() {
+        mHandler.removeCallbacks(mRunnable)
+    }
+
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
